9de0298c8f53def33f5c7b4eca581bc33824926a
codeek89 <codeek89@gmail.com>
2021-03-06 20:51:05 +0100

    setting portrait mode for every screen.

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,14 @@ import 'package:dragonballhub/screens/settings_screen.dart';
 import 'package:dragonballhub/themedata.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
   // ProviderScope is where the state of our providers will be stored.
   runApp(ProviderScope(child: MyApp()));
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -20,6 +20,7 @@ class LoginScreen extends StatefulWidget {
 
 
 class _LoginScreenState extends State<LoginScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
